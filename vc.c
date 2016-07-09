8ad2d19390dd4f7a9721272fd333a4829cf50a23
DEEPASUMATHI <deepasumathi114@gmail.com>
2016-07-09 10:17:03 +0530

    Create vc.c

---
+++ vc.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+char c,arr[10]={'a','e','i','o','u','A','E','I','O','U'};
+scanf("%c",c);
+for(int i=0;i>9;i++)
+if(c==arr[i])
+ printf("Vowels");
+else
+ printf("Consonants");
+}
